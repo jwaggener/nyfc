98b7d75c9436814a24cd60651be8dad9f40ad384
John Waggener <JohnWaggener@unknown002608e9cddc.gateway.2wire.net>
2013-08-01 19:58:23 -0400

    adding descriptive comment for an nyfc obj

--- db/nyfc.js
+++ db/nyfc.js
@@ -1,6 +1,22 @@
 /*
 * nyfc - Name Your Favorite Color
 * This database contains our nyfc objects
+*	id
+*	name - string. name of color given by user
+*	color: {
+*	hex: ffffff - string
+*	rgb: {r: 255, g: 255, b:255} - numbers
+*	hsl: {h:1,s:1,l:1} hue saturation, lightness, values from 0 - 1
+*	}hex value.
+*	author - unique user who created this name/color pairing
+*	adult content - Boolean
+*	font: {
+*	font-family
+*	font-size
+*	font-style
+*	font-weight
+*	font-variant
+*	}
 */
 var cradle = require('cradle');
 
