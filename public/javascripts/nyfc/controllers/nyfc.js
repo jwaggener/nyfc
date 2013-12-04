ec60cfe625e936a8c26db4c5faa1a98f6badb09e
John Waggener <john.waggener@gmail.com>
2013-12-04 01:02:08 -0500

    getting the angular bit going. stubbing out the service to retrieve the documents

---
+++ public/javascripts/nyfc/controllers/nyfc.js
@@ -0,0 +1,5 @@
+window.NYFC.controllers.NYFCMain = function ($scope, NYFCCouchDBService) {
+	console.log('$scope', $scope);
+	console.log('NYFCCouchDBService', NYFCCouchDBService);
+	console.log('NYFCCouchDBService.query()', NYFCCouchDBService.query());
+}
---
+++ public/javascripts/nyfc/nyfc.js
@@ -0,0 +1,3 @@
+// Create a module to support our shopping views
+window.NYFC = angular.module('NYFC', []);
+window.NYFC.controllers = {};
