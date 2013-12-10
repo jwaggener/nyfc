658cb8f4a0931480b6ee358cafa3448224b50a56
John Waggener <john.waggener@gmail.com>
2013-12-09 22:59:10 -0500

    added default color for the preview

--- app/scripts/directives/nyfc_spectrum.js
+++ app/scripts/directives/nyfc_spectrum.js
@@ -3,12 +3,12 @@
 //this is a wrapper for the spectrum color picker.
 angular.module('nyfcApp')
 	.directive('nyfcSpectrum', function(){
-	
 	return {
 		restrict: 'EA',
 		template: '<div></div>',
 		replace: true,
 		link: function ($scope, element, attrs) {
+			$scope.selectedRgbString = 'rgb(0,0,255)';
 			$(element[0]).spectrum({
 				color: '#00f',
 				flat: true,
